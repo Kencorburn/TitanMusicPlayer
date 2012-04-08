611748e67a0d60e11c25154e40cdc36ab0d5b531
Kendall <Kendall@192.168.1.138>
2012-04-08 14:11:43 -0400

    Library

---
+++ test/LibraryTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.TitanPlayer.BLL.Playlist;
+import com.TitanPlayer.BLL.Song;
+import com.TitanPlayer.BLL.Library;
+import java.util.Scanner;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kendall
+ */
+public class LibraryTest {
+    
+    public LibraryTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+   @Test
+   public void emptyLibraryTest(){
+       Library emptyLibrary = new Library();
+       assertEquals(0, emptyLibrary.songCount());
+       
+   }
+   @Test
+   public void addSongTest(){
+       Library myLibrary = new Library();
+       myLibrary.addSong(new Song("My Posse's on Broadway.", "Sir Mix-a-Lot"));
+       myLibrary.addSong(new Song("Brass Monkey", "Beastie Boys"));
+       assertEquals(2, myLibrary.songCount());
+       
+   }
+   @Test
+   public void removeSongTest(){
+       Library myLibrary = new Library();
+       myLibrary.removeSong(new Song("My Posse's on Broadway.", "Sir Mix-a-Lot"));
+       myLibrary.removeSong(new Song("Brass Monkey", "Beastie Boys"));
+       assertEquals(0, myLibrary.songCount());
+   }
+ 
+}
+
